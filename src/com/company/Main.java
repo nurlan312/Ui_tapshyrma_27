77232dc49b4a3363422eefcf30fa5390ca311c82
nurlan312 <96204488+nurlan312@users.noreply.github.com>
2021-12-16 23:28:19 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,10 @@ import javax.swing.plaf.InsetsUIResource;
 public class Main {
 
     public static void main(String[] args) {
-        Box box = new Box<>(12, 6, 20);
-        System.out.println(box.methodBox(box));
+//         Box box = new Box(12, 6, 20);
+//         System.out.println(box.methodBox(box));
+        
+        Box<Integer, Integer, Integer> box = new Box<>(12, 6, 20);
+        System.out.println(java.util.Optional.ofNullable(box.methodBox(box)));
     }
 }
